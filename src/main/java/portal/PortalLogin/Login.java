af2702ce08fbb45f715ead088a0ae34ddce21bd9
Tonisha Whyte <tonisha.whyte@hp.com>
2015-04-28 13:07:15 -0700

    automate portal login on dev stack

--- src/main/java/portal/PortalLogin/Login.java
+++ src/main/java/portal/PortalLogin/Login.java
@@ -9,6 +9,13 @@ import java.io.UnsupportedEncodingException;
 
 import com.google.gson.stream.JsonReader;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 /**
  * Log in to the portal.   
@@ -16,17 +23,24 @@ import com.google.gson.stream.JsonReader;
  */
 public class Login
 {
+    private WebDriver driver;
+
 	private String username;
 	private String password; 
+	private String home; 
+
     private String devStackProperty = "dev_stack";
-    private String devStackUserName = "username"; 
+	private String devStackUserName = "username"; 
     private String devStackPassword = "password";
+    private String devStackHome = "home";
 
     /**
      * Set up the username an password for logging into the portal on a dev stack account.
      */
 	public Login() {
+		// read the credentials for the dev stack from a config file
 		try {
+
 			// JsonReader is NOT a convenient Json parser.  Pick a different one!
 			JsonReader jsonReader = new JsonReader(new InputStreamReader(
 					new FileInputStream("src/main/config/settings.json"), "UTF-8"));
@@ -42,6 +56,9 @@ public class Login
                        }
                        else if (propertyName.equals(devStackPassword)){
                     	   this.password = jsonReader.nextString();
+                       }
+                       else if (propertyName.equals(devStackHome)){
+                    	   this.home = jsonReader.nextString();
                        }
                        else {
                     	   jsonReader.skipValue();
@@ -62,12 +79,53 @@ public class Login
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
+		
+		// create new instance of chrome driver
+		this.driver = new ChromeDriver();
+		driver.get(this.home);
+	}
+
+    public WebDriver getDriver() {
+		return driver;
 	}
 	
-	public 
+	public WebElement findSignInButton() {
+		return driver.findElement(By.id("signInLink"));
+	}
+	
+	private WebElement findUserNameField() {
+		return (new WebDriverWait(driver, 5))
+		  .until(ExpectedConditions.presenceOfElementLocated(By.id("emailid")));
+	}
+	
+	private WebElement findPasswordField() {
+		return driver.findElement(By.id("passwordid"));
+	}
+
+	private WebElement findSignInSubmitButton() {
+		return driver.findElement(By.id("signInSubmit"));
+	}
+	
+	public void enterCredentials(){
+	   findUserNameField().sendKeys(username);	
+	   findPasswordField().sendKeys(password);	
+	}
+	
+	public WebElement findSignedInDropDownMenu(){
+		return (new WebDriverWait(driver, 5))
+		  .until(ExpectedConditions.presenceOfElementLocated(By.id("dropdownMenu1")));
+	}
 
     public static void main( String[] args )
     {
-        Login l = new Login();
+        Login login = new Login();
+        WebDriver driver = login.getDriver();
+        
+        login.findSignInButton().click();
+        login.enterCredentials();
+        login.findSignInSubmitButton().click();
+		Assert.assertEquals(true, login.findSignedInDropDownMenu().isDisplayed());
+        
+        driver.close(); 
     }
 }
